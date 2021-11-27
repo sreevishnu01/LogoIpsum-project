f9b5f9cf16d2bee2662c4935fe966c562966146f
vishnu <sreevishnu01@gmail.com>
2021-11-27 21:22:35 +0400

    carousel

--- clint/src/components/header/Carousels.jsx
+++ clint/src/components/header/Carousels.jsx
@@ -4,11 +4,20 @@ import { useState } from 'react'
 
 function Carousels() {
 
-    const randomImg = {
-        imagel: 'https://picsum.photos/1620/612',
-        images: 'https://picsum.photos/1620/610',
+    const randomImg = [
+        {
+            "image": "./images/Rectangle.png"
+        }, {
+            "image": "./images/placeholder.png"
+        }
 
-    }
+        , {
+            "image": "./images/placeholder1.png"
+        }
+
+    ]
+    // imagel: 'https://picsum.photos/1620/612',
+
 
     const [index, setIndex] = useState(0);
 
@@ -17,62 +26,70 @@ function Carousels() {
     };
     return (
 
-        <>
-            <Container>
-                <div className="carousel">
-                    <img src="./images/Rectangle.png" alt="aa" />
-                </div>
-            </Container>
-        </>
-
-        // <Container>
-        //     <div >
-        //         <Carousel activeIndex={index} onSelect={handleSelect} className="carousel">
-        //             <Carousel.Item>
-        //                 <img
-        //                     className="d-block w-100"
-        //                     src="./images/Rectangle.png"
-        //                     alt="First slide"
-        //                 />
-        //                 <Carousel.Caption>
-        //                     <h3>From students to senior citizens</h3>
-        //                     <p>this web portal of</p>
-        //                     <h3> "Products and Classifieds”</h3>
-        //                     <p>provides it all</p>
-        //                 </Carousel.Caption>
-        //             </Carousel.Item>
-        //             <Carousel.Item>
-        //                 <img
-        //                     className="d-block w-100"
-        //                     src="./images/placeholder.png"
-        //                     alt="Second slide"
-        //                 />
-
-        //                 <Carousel.Caption>
-        //                     <h3>From students to senior citizens</h3>
+        // <>
+        //     <Container>
+        //         <div className="carousels">
+        //             {randomImg.map((item, index) => (
+        //                 <div key={index} className="carousel">
+        //                     <img src={item.image} alt="aa" />
         //                     <p>From students to senior citizens
         //                         this web portal of
         //                         "Products and Classifieds”
         //                         provides it all</p>
-        //                 </Carousel.Caption>
-        //             </Carousel.Item>
-        //             <Carousel.Item>
-        //                 <img
-        //                     className="d-block w-100"
-        //                     src="./images/placeholder1.png"
-        //                     alt="Third slide"
-        //                 />
+        //                 </div>
+        //             ))}
 
-        //                 <Carousel.Caption>
-        //                     <h3>Third slide label</h3>
-        //                     <p>
-        //                         Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-        //                     </p>
-        //                 </Carousel.Caption>
-        //             </Carousel.Item>
-        //         </Carousel>
-        //     </div>
-        // </Container>
+        //         </div>
+        //     </Container>
+        // </>
+
+        <Container>
+            <div >
+                <Carousel activeIndex={index} onSelect={handleSelect} className="carousel">
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 carousel-img"
+                            src="./images/Rectangle.png"
+                            alt="First slide"
+                        />
+                        <Carousel.Caption className="carousel-cap">
+                            <p>From students to senior citizens</p>
+                            <p>this web portal of</p>
+                            <h1> "Products and Classifieds”</h1>
+                            <p>provides it all</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 carousel-img"
+                            src="./images/placeholder.png"
+                            alt="Second slide"
+                        />
+
+                        <Carousel.Caption className="carousel-cap">
+                            <h3>From students to senior citizens</h3>
+                            <p>this web portal of</p>
+                            <h3> "Products and Classifieds”</h3>
+                            <p>provides it all</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100 carousel-img"
+                            src="./images/placeholder1.png"
+                            alt="Third slide"
+                        />
+
+                        <Carousel.Caption className="carousel-cap">
+                            <h3>From students to senior citizens</h3>
+                            <p>this web portal of</p>
+                            <h3> "Products and Classifieds”</h3>
+                            <p>provides it all</p>
+                        </Carousel.Caption>
+                    </Carousel.Item>
+                </Carousel>
+            </div>
+        </Container>
 
     )
 }
