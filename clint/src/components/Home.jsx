03e15759dc18b744f76e5c9c28c0b50297caa52c
sreevishnu01 <sreevishnu01@gmail.com>
2022-02-01 14:39:36 +0400

    react slick caruosel and sliding dots css, old is not deleted. Dinamic task is pending..

--- clint/src/components/Home.jsx
+++ clint/src/components/Home.jsx
@@ -1,19 +1,21 @@
 import Carousels from './header/Carousels'
 import Navbar1 from './topbar/Navbar'
 import Main from './main/main'
+import CarouselSlick from './header/CarouselSlick'
 // import axios from '../Assets/axiosconfig'
 
 
 
 function Home() {
-    
 
-    
+
+
 
     return (
         <>
             <Navbar1 />
-            <Carousels />
+            {/* <Carousels /> */}
+            <CarouselSlick />
             <Main />
         </>
     )
---
+++ clint/src/components/header/CarouselSlick.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Container, Card } from 'react-bootstrap';
+import Slider from "react-slick";
+
+function CarouselSlick() {
+    const settings = {
+        className: "carousel",
+        centerMode: true,
+        dots: true,
+        infinite: true,
+        centerPadding: "160px",
+        slidesToShow: 1,
+        speed: 500,
+        autoplay: false,
+        autoplaySpeed: 5000,
+        arrows: false,
+        customPaging: i => {
+            return (
+                <span>&nbsp; </span>
+            )
+        }
+    };
+    return <div>
+        <Container fluid className='p-0'>
+            <Slider {...settings}>
+                <Card className="border-none text-white">
+                    <Card.Img src="./images/Rectangle.png" alt="Card image" className='carousel_img' />
+                    <Card.ImgOverlay className="carousel-cap">
+                        <p>From students to senior citizens</p>
+                        <p>this web portal of</p>
+                        <h1> "Products and Classifieds”</h1>
+                        <p>provides it all</p>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card className="border-none text-white">
+                    <Card.Img src="./images/placeholder.png" alt="Card image" className='carousel_img' />
+                    <Card.ImgOverlay className="carousel-cap">
+                        <p>From students to senior citizens</p>
+                        <p>this web portal of</p>
+                        <h1> "Products and Classifieds”</h1>
+                        <p>provides it all</p>
+                    </Card.ImgOverlay>
+                </Card>
+                <Card className="border-none text-white">
+                    <Card.Img src="./images/placeholder1.png" alt="Card image" className='carousel_img' />
+                    <Card.ImgOverlay className="carousel-cap">
+                        <p>From students to senior citizens</p>
+                        <p>this web portal of</p>
+                        <h1> "Products and Classifieds”</h1>
+                        <p>provides it all</p>
+                    </Card.ImgOverlay>
+                </Card>
+            </Slider>
+        </Container>
+
+    </div>;
+}
+
+export default CarouselSlick;
